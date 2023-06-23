7468b32ac5b6181d198e8afb2982d24b90cf2ff6
Antoine P <antoine.plaineau@etudiant.univ-lr.fr>
2023-06-23 17:16:00 +0200

    feat(header): create component header with style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,34 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+
 export default function Header() {
-    return <header>This is a header</header>;
+    return (
+        <header>
+            <img src={logo} alt="Logo Kasa"></img>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                isActive ? "active" : ""
+                            }
+                        >
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive }) =>
+                                isActive ? "active" : ""
+                            }
+                        >
+                            A Propos
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
